b2c8672b43273f5a0f4a6dd73ea34a7de99f356d
Muktar627 <muktarruet08@gmail.com>
2024-11-30 13:12:02 +0600

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,11 +1,4 @@
-"""
-git init
-git add . #(All file and folder)
-git commit -m "first commit" #(message)
-git branch -M main
-git remote add origin https://github.com/Muktar627/WebDev.git
-git push -u origin main
-"""
-
-print("Git_Test")
-
+print("Hello python")
+print("Hello python")
+print("Hello python")
+print("Hello python")
